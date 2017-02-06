4ba368afe9975f0e4dc50c320aeb872f239e59b3
root <root@localhost.localdomain>
2017-02-06 11:58:50 -0500

    add new files

---
+++ curl/thread_safe_obj_pool.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <boost/lockfree/queue.hpp>
+#include <boost/pool/object_pool.hpp>
+
+
+template <typename T>
+class ThreadSafeObjPool 
+{
+public:
+	ThreadSafeObjPool() : max_size_(0), free_queue_(0) {}
+	~ThreadSafeObjPool() { clear(); }
+
+	bool init(int max_size) {
+		if (max_size <= 0)
+			return false;
+
+		free_queue_.reserve(max_size);
+		T* p = NULL;
+		for (int i=0; i<max_size; i++) {
+			p = obj_pool_.malloc();
+			free_queue_.push(p);
+		}
+
+		max_size_ = max_size;
+		return true;
+	}
+
+	void clear() {
+		T* p = NULL;
+		while (true) {
+			if (free_queue_.empty())
+				break;
+			if (!free_queue_.pop(p))
+				break;
+			obj_pool_.destroy(p);
+		}
+	}
+
+	T* malloc() {
+		T* p = NULL;
+		if (!free_queue_.pop(p))
+			return NULL;
+
+		return p;
+	}
+
+	bool free(T* p) {
+		return free_queue_.push(p);
+	}
+
+	bool hasFree() {
+		return !free_queue_.empty();
+	}
+
+private:
+	int max_size_;	
+	boost::lockfree::queue<T*> free_queue_;
+	boost::object_pool<T> obj_pool_;
+};
