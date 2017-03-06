a50b94dd62453020e47b473ea80d4bfc971d59ae
huoshan017 <huoshan017@localhost.localdomain>
2017-03-06 19:41:35 +0800

    mm

---
+++ asio/libjmy/jmy_net_tool.cpp
@@ -0,0 +1,109 @@
+#include "jmy_net_tool.h"
+
+JmyNetTool::JmyNetTool()
+	: up_stream_(0),
+	down_stream_(0),
+	up_last_stream_(0),
+	down_last_stream_(0),
+	last_up_bps_(0),
+	last_down_bps_(0),
+	last_bps_(0),
+	interval_(1),
+	state_(STATE_STOP)
+{
+}
+
+JmyNetTool::~JmyNetTool()
+{
+}
+
+void JmyNetTool::reset()
+{
+	up_stream_ = 0;
+	down_stream_ = 0;
+	state_ = STATE_STOP;
+}
+
+bool JmyNetTool::start()
+{
+	if (state_ != STATE_STOP)
+		return false;
+	state_ = STATE_START;
+	return true;
+}
+
+bool JmyNetTool::pause()
+{
+	if (state_ != STATE_START)
+		return false;
+	state_ = STATE_PAUSE;
+	pause_tick_ = std::chrono::system_clock::now();
+	return true;
+}
+
+bool JmyNetTool::resume()
+{
+	if (state_ != STATE_PAUSE)
+		return false;
+	state_ = STATE_START;
+	return true;
+}
+
+bool JmyNetTool::stop()
+{
+	if (state_ != STATE_START)
+		return false;
+	state_ = STATE_STOP;
+	return true;
+}
+
+void JmyNetTool::addUpStream(uint32_t count)
+{
+	if (state_ != STATE_START)
+		return;
+	up_stream_ += count;
+}
+
+void JmyNetTool::addDownStream(uint32_t count)
+{
+	if (state_ != STATE_START)
+		return;
+	down_stream_ += count;
+}
+
+uint32_t JmyNetTool::getSendBps()
+{
+	auto now = std::chrono::system_clock::now();
+	auto d = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_up_tick_).count();
+	uint32_t bps = 0;
+	if (d >= interval_ * 1000) {
+		bps = (up_stream_ - up_last_stream_) * 1000 / d;
+		up_last_stream_ = up_stream_;
+		last_up_bps_ = bps;
+		last_up_tick_ = now;
+	} else {
+		bps = last_up_bps_;
+	}
+	return bps;
+}
+
+uint32_t JmyNetTool::getRecvBps()
+{
+	auto now = std::chrono::system_clock::now();
+	auto d = std::chrono::duration_cast<std::chrono::milliseconds>(now - last_down_tick_).count();
+	uint32_t bps = 0;
+	if (d >= interval_ * 1000) {
+		bps = (down_stream_ - down_last_stream_) * 1000 / d;
+		down_last_stream_ = down_stream_;
+		last_down_bps_ = bps;
+		last_down_tick_ = now;
+	} else {
+		bps = last_down_bps_;
+	}
+	return bps;
+}
+
+uint32_t JmyNetTool::getBps()
+{
+	return getSendBps() + getRecvBps();
+}
---
+++ asio/libjmy/jmy_net_tool.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <stdint.h>
+#include <chrono>
+
+class JmyNetTool
+{
+public:
+	JmyNetTool();
+	~JmyNetTool();
+
+	void reset();
+
+	bool start();
+	bool pause();
+	bool resume();
+	bool stop();
+
+	void addUpStream(uint32_t count);
+	void addDownStream(uint32_t count);
+
+	uint32_t getSendBps();
+	uint32_t getRecvBps();
+	uint32_t getBps();
+
+private:
+	uint64_t up_stream_;
+	uint64_t down_stream_;
+	uint32_t up_last_stream_;
+	uint32_t down_last_stream_;
+	uint32_t last_up_bps_;
+	uint32_t last_down_bps_;
+	uint32_t last_bps_;
+	std::chrono::system_clock::time_point start_;
+	std::chrono::system_clock::time_point pause_tick_;
+	std::chrono::system_clock::time_point last_up_tick_;
+	std::chrono::system_clock::time_point last_down_tick_;
+	uint32_t interval_; // time span to get bps in seconds
+	enum State {
+		STATE_STOP = 1,
+		STATE_START = 2,
+		STATE_PAUSE = 3,
+	} state_;
+};
