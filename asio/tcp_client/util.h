517df2f14d24f95076108ae016ef65b56eae4e1f
root <root@localhost.localdomain>
2017-03-07 12:01:21 -0500

    add tcp_client/util.h

---
+++ asio/tcp_client/util.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../libjmy/jmy_log.h"
+#include "config_data.h"
+
+#define ClientLogDebug(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_DEBUG, __VA_ARGS__)
+
+#define ClientLogInfo(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_INFO, __VA_ARGS__)
+
+#define ClientLogNotice(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_NOTICE, __VA_ARGS__)
+
+#define ClientLogWarn(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_WARN, __VA_ARGS__)
+
+#define ClientLogError(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_ERROR, __VA_ARGS__)
+
+#define ClientLogFatal(...) \
+	JmyLog::getInstance()->log(s_log_cate_name, __FILE__, sizeof(__FILE__)-1, \
+			__func__, sizeof(__func__)-1, __LINE__, ZLOG_LEVEL_FATAL, __VA_ARGS__)
