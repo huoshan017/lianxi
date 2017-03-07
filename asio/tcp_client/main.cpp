5cb9602f699bec8f776348f2ddf8c4d9be4e4ecb
huoshan017 <huoshan017@localhost.localdomain>
2017-03-07 17:57:38 +0800

    mm

--- asio/tcp_client/main.cpp
+++ asio/tcp_client/main.cpp
@@ -85,7 +85,6 @@ int main(int argc, char* argv[])
 			std::cout << "connector run failed" << std::endl;
 			break;
 		}
-		sleep(1);
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	} 
 
