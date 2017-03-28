3e5c8d12a9fac2fd52dd5cdf5e659a077e623527
root <root@localhost.localdomain>
2017-03-28 12:32:55 -0400

    mm

--- jmy_game/proto/gen_proto.sh
+++ jmy_game/proto/gen_proto.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=../servers/libjmy/thirdparty/lib/protobuf
+#export LD_LIBRARY_PATH=../servers/libjmy/thirdparty/lib/protobuf
 ../servers/libjmy/thirdparty/bin/protoc --cpp_out=./ ./common.proto
