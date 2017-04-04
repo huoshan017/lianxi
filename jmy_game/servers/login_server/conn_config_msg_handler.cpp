2d7078c0853a9bc68e04fd93a95491bd86d36c8b
root <root@localhost.localdomain>
2017-04-03 22:56:23 -0400

    mm

--- jmy_game/servers/login_server/conn_config_msg_handler.cpp
+++ jmy_game/servers/login_server/conn_config_msg_handler.cpp
@@ -1,6 +1,14 @@
 #include "conn_config_msg_handler.h"
+#include "../libjmy/jmy_datatype.h"
+#include "../common/util.h"
+#include "../../proto/src/server.pb.h"
 
 int ConnConfigMsgHandler::processConnectResponse(JmyMsgInfo* info)
 {
+	MsgCS2LS_ConnectResponse response;
+	if (!response.SerializeToArray(info->data, info->len)) {
+		ServerLogError("serilize config_server to login_server connect response message failed");		
+		return -1;
+	}	
 	return 0;
 }
