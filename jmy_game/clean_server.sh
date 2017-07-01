b892c3822d809e49de804433f96a719efa2bc09a
huoshan017 <huoshan017@localhost.localdomain>
2017-07-01 20:22:55 +0800

    mm

--- jmy_game/clean_server.sh
+++ jmy_game/clean_server.sh
@@ -5,5 +5,6 @@ rm servers/db_server/db_tables_func.*
 rm servers/db_server/db_tables_struct.*
 rm servers/game_server/csv_list_manager.h
 rm ./servers/bin/db_generator/db_generator
+rm ./servers/bin/csv_generator/csv_generator
 cd ./servers
 make clean
